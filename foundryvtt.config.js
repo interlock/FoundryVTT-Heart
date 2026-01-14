440022ea8f004e00eff8c4120e2a078a00da5b9a
interlock <interlock@users.noreply.github.com>
2026-01-13 16:53:30 -0800

    add support for fountry vtt 13

--- foundryvtt.config.js
+++ foundryvtt.config.js
@@ -6,6 +6,7 @@ module.exports = {
     description: 'A red wet heaven slumbers fitful under the city of Spire. Purchase at https://rowanrookanddecard.com/product/heart-the-city-beneath-rpg/',
     compatibility: {
         "minimum": "12",
-        "verified": "12"
+        "verified": "13",
+        "maximum": "13"
     },
 };
